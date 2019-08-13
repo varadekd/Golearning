f489ce5ec07205f9d4b10ccb42f64044d11bb08c
Varadekushagra <kushagra@robustest.com>
2019-08-13 11:02:40 +0530

    Inital commit

---
+++ src/Excersie1/ex1.go
@@ -0,0 +1,22 @@
+// Question : Assign the values to variables x , y and z using short declartion operator
+// values to be used are 42 , "James Bond" , true
+// print values using single print and multiple print statements
+
+package main
+
+import "fmt"
+
+func main() {
+	// Declaration of variable using short hand decalration
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// Single print statement
+	fmt.Println(x, y, z)
+
+	// Multiple print statement
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
