567cc2864c38cc427e0edacc01b5ae99b368bf7c
Varadekushagra <kushagra@robustest.com>
2019-08-13 11:22:10 +0530

    Excersie 2

---
+++ src/Excersie/Operators.go
@@ -0,0 +1,20 @@
+// Use the following operators == , <=, >= , !=, < , >
+
+package main
+
+import "fmt"
+
+func main() {
+	a := (42 == 42)
+	b := (2 <= 2)
+	c := (4 >= 4)
+	d := (6 != 6)
+	e := (8 > 6)
+	f := (10 < 12)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
